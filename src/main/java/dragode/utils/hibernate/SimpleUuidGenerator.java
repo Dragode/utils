a7f25eaeee1966f16db2b5f35db6cdf49ec281d7
俞建龙 <300116@nd.com>
2017-06-17 17:21:41 +0800

    新增hibernate主键生成wrapper

---
+++ src/main/java/dragode/utils/hibernate/SimpleUuidGenerator.java
@@ -0,0 +1,47 @@
+package dragode.utils.hibernate;
+
+import org.hibernate.HibernateException;
+import org.hibernate.engine.spi.SessionImplementor;
+import org.hibernate.id.GUIDGenerator;
+import org.hibernate.id.IdentifierGenerator;
+
+import java.io.Serializable;
+import java.lang.reflect.Field;
+
+/**
+ * 如果主键显式设定了值，则用设定的值，不自动生成ID
+ *
+ * @author Dragode
+ */
+//TODO 写TestCase
+public class SimpleUuidGenerator extends GUIDGenerator {
+
+    private String idField;
+    private IdentifierGenerator identifierGenerator;
+
+    public SimpleUuidGenerator(String idField, IdentifierGenerator identifierGenerator) {
+        this.idField = idField;
+        this.identifierGenerator = identifierGenerator;
+    }
+
+    @Override
+    public Serializable generate(SessionImplementor session, Object entity) throws HibernateException {
+        if (entity != null) {
+            try {
+                Field declaredField = entity.getClass().getDeclaredField(idField);
+                Object value = declaredField.get(entity);//TODO 是否是这样获取属性？如果entity是null，会怎么样？
+                if (value != null
+                        && value instanceof Serializable) {
+                    return (Serializable) value;
+                }
+            } catch (NoSuchFieldException e) {
+                //不存在idField对应的属性，则用GUIDGenerator生成
+            } catch (IllegalAccessException e) {
+                return super.generate(session, entity);
+                //不存在idField对应的属性，则用GUIDGenerator生成
+            }
+        }
+
+        return identifierGenerator.generate(session, entity);
+    }
+}
